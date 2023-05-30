9fc53ff04117b776688c00ab363ce942e844660b
이승훈 <seunghun@iseunghun-ui-MacBookPro.local>
2023-05-30 21:30:30 +0900

    addressExtractByGPT 로직 수정 및 imageUrlToBuffer 추가

--- util.ts
+++ util.ts
@@ -2,7 +2,7 @@ import {ImageAnnotatorClient} from '@google-cloud/vision';
 import AWS from 'aws-sdk';
 import axios from 'axios';
 
-/** DB 조회 **/
+/** DB 쿼리 실행 **/
 export async function queryMySQL(connection: any, query: string, values: any): Promise<any> {
     try {
         const [rows] = await connection.execute(query, values);
@@ -40,7 +40,7 @@ export async function callVisionAPI(imageUrl: string): Promise<object>{
 }
 
 /** 영수증 사진인지 확인 **/
-export async function isReceipt(imageBuffer): Promise<boolean>{
+export async function isReceipt(imageBuffer: Buffer): Promise<boolean>{
     try{
         const rekognition = new AWS.Rekognition();
         const params = {
@@ -68,20 +68,48 @@ export async function isReceipt(imageBuffer): Promise<boolean>{
 }
 
 /** GPT API 주소 추출 **/
-export async function addressExtract(text: string): Promise<string>{
-    let extractedAddress: string = '';
-    const GPT_API_KEY = process.env.GPT_API_KEY;
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-        prompt: text,
-        max_tokens: 100,
-        temperature: 0.7
-    }, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${GPT_API_KEY}`
-        }
-    });
+export async function addressExtractByGPT(text: string): Promise<any>{
+    try{
+        const GPT_API_KEY: string = process.env.GPT_API_KEY;
+        const response = await axios.post(
+            'https://api.openai.com/v1/engines/davinci-codex/completions',
+            {
+                prompt: `Extract the address from the following text:\n${text}`,
+                max_tokens: 100,
+                temperature: 0.7,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${GPT_API_KEY}`,
+                },
+            }
+        );
 
-    return response.data.choices[0].text.trim();
+        console.log("addressExtractByGPT response", response.data);
+        if(!response.data || (response.data && !response.data.choices) || (response.data && response.data.choices && response.data.choices.length === 0)) {
+            console.log("Not exist response.data.choices");
+            return false;
+        }
+
+        return response.data.choices[0].text.trim();
+    }catch (e) {
+        console.log("Error in addressExtract", e);
+        return false;
+    }
 }
 
+/** URL을 Buffer 데이터로 변환 **/
+async function imageUrlToBuffer(imageUrl): Promise<Buffer> {
+    try {
+        const response = await axios.get(imageUrl, {
+            responseType: 'arraybuffer',
+        });
+
+        const imageBuffer: Buffer = Buffer.from(response.data, 'binary');
+        return imageBuffer;
+    } catch (error) {
+        console.log('Error in imageUrlToBuffer:', error);
+        return null;
+    }
+}
